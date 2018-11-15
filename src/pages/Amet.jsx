099459c9f92cea211776bd7c66144db86afed817
bakito <github@bakito.ch>
2018-11-15 23:23:52 +0100

    reformat with prettier

--- src/pages/Amet.jsx
+++ src/pages/Amet.jsx
@@ -6,10 +6,7 @@ import { renderAdditionalInfoItems, renderActions } from './util/listViewUtils';
 const action1 = rowNum => alert(`Action 1 executed on Row ${rowNum}`);
 const action2 = rowNum => alert(`Action 2 executed on Row ${rowNum}`);
 
-const rowActions = [
-  { label: 'Action 1', fn: action1 },
-  { label: 'Action 2', fn: action2 }
-];
+const rowActions = [{ label: 'Action 1', fn: action1 }, { label: 'Action 2', fn: action2 }];
 
 const AmetPage = () => (
   <Grid fluid className="container-pf-nav-pf-vertical">
@@ -24,33 +21,21 @@ const AmetPage = () => (
       </Grid.Col>
     </Grid.Row>
     <ListView>
-      {expandableListItems.map(
-        (
-          {
-            icon,
-            title,
-            description,
-            properties,
-            actions,
-            expandedContentText
-          },
-          index
-        ) => (
-          <ListView.Item
-            key={index}
-            checkboxInput={<input type="checkbox" />}
-            leftContent={<ListView.Icon name={icon} />}
-            additionalInfo={renderAdditionalInfoItems(properties)}
-            actions={renderActions(rowActions, index)}
-            heading={title}
-            description={description}
-          >
-            <div className="row">
-              <div className="col-md-12">{expandedContentText}</div>
-            </div>
-          </ListView.Item>
-        )
-      )}
+      {expandableListItems.map(({ icon, title, description, properties, actions, expandedContentText }, index) => (
+        <ListView.Item
+          key={index}
+          checkboxInput={<input type="checkbox" />}
+          leftContent={<ListView.Icon name={icon} />}
+          additionalInfo={renderAdditionalInfoItems(properties)}
+          actions={renderActions(rowActions, index)}
+          heading={title}
+          description={description}
+        >
+          <div className="row">
+            <div className="col-md-12">{expandedContentText}</div>
+          </div>
+        </ListView.Item>
+      ))}
     </ListView>
   </Grid>
 );
